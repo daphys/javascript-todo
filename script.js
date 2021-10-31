f3e28126b24242f8d001bd4addebbb4eb429a550
Vlad <69009463+daphys@users.noreply.github.com>
2021-10-31 15:24:23 +0300

    update1.1

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ let arr = [];
 let text = document.getElementById('main');
 
 function makeToList() {
+    let newArr = new Array()
+    newArr.push(inputTo.value);
+    let newElem = document.createElement('div');
+    newElem.className = 'newElement';
+    newElem.innerHTML = newArr
+
+    console.log(newArr);
+
     
-    if (inputTo.value == 0) {
-        alert('Добавьте пожалуйста вашу задачу');
-    } else{
-        arr.push(inputTo.value);
-        text.innerHTML = `${arr} </br>`;  
-    }
+    
+    // if (inputTo.value == 0) {
+    //     alert('Добавьте пожалуйста вашу задачу');
+    // } else{
+    //     arr.push(inputTo.value);
+    //     text.innerHTML = `${arr} </br>`;  
+    // }
 }
 
 buttonTo.addEventListener('click', makeToList);
