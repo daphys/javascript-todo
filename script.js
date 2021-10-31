2fab13b2100be76f7c9619995d486955d421ec0d
Vlad <floppahear@gmail.com>
2021-10-31 18:22:27 +0300

    update1.2

--- script.js
+++ script.js
@@ -4,22 +4,26 @@ let arr = [];
 let text = document.getElementById('main');
 
 function makeToList() {
-    let newArr = new Array()
-    newArr.push(inputTo.value);
-    let newElem = document.createElement('div');
-    newElem.className = 'newElement';
-    newElem.innerHTML = newArr
-
-    console.log(newArr);
-
+    // let newArr = new Array()
+    // newArr.push(inputTo.value);
+    // let newElem = document.createElement('div');
+    // newElem.className = 'newElement';
+    // document.body.append(newElem);
+    // newElem.innerHTML = newArr;
+    // console.log(newArr);
     
-    
-    // if (inputTo.value == 0) {
-    //     alert('Добавьте пожалуйста вашу задачу');
-    // } else{
-    //     arr.push(inputTo.value);
-    //     text.innerHTML = `${arr} </br>`;  
-    // }
+    if (inputTo.value == 0) {
+        alert('Добавьте пожалуйста вашу задачу');
+    } else{
+        let newArr = new Array()
+        newArr.push(inputTo.value);
+        let newElem = document.createElement('div');
+        newElem.className = 'newElement';
+        document.body.append(newElem);
+        newElem.innerHTML = newArr;
+        console.log(newArr);
+    }
 }
+
 
 buttonTo.addEventListener('click', makeToList);
